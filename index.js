e6d888d0d3c2f062c251bc1a94c87b3f4421cb34
Carlos Alegre <chrlyalegre6@gmail.com>
2021-03-11 14:39:42 -0300

    Se agregaron el resto de las funcionalida

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ like.addEventListener("click", e => {
     document.getElementById("like").src = "./imgs/red-heart.png"
 });
 
+// Me gusta logo (sólo en desktop)
+const tap = document.querySelector(".inner-circle")
+tap.addEventListener("dblclick", e => {
+    document.getElementById("conteo").innerHTML = `Le diste 1 me gusta, ¡gracias!`;
+    document.getElementById("like").src = "./imgs/red-heart.png"
+})
+
+
+/*************************
+ * Sección de comentarios
+ *************************/
+
 // Despliega la sección de comentarios
 const showMenu = document.getElementById("comentar")
 showMenu.addEventListener("click", e => {
-    const menu = document.getElementById("comment-section")
+    const menu = document.getElementById("comment-writing")
     menu.style.display = "flex"
 })
 
@@ -18,22 +30,48 @@ publicar.addEventListener("click", e => {
     const parent = document.getElementById("comment-section")
     const comment = document.createElement("p")
     comment.style.textAlign = "left"
+    comment.style.marginTop = "0"
     comment.textContent = document.getElementById("text-comment").value
-    parent.insertAdjacentElement("beforebegin", comment)
+    parent.insertAdjacentElement("beforeend", comment)
+    comment.scrollIntoView()
+    
     console.log(comment)
 });
+
+// Color palabra publicar
+const enviar = document.getElementById("text-comment")
+enviar.addEventListener("keypress", e => {
+    const publicar = document.getElementById("publicar")
+    if (document.getElementById("text-comment").value.length > -1) {
+        publicar.style.color = "rgb(78, 167, 226)"
+    }
+    publicar.onlick = () => {
+        text.value = "";
+    }
+})
+
+// Descomentar
+
+
+
+/********************
+ * Sección de guardar
+ ********************/
 
 // Guarda en colección
 const save = document.getElementById("save")
 save.addEventListener("click", e => {
-    document.getElementById("save").src = "./imgs/black-save.png"
+    save.src = "./imgs/black-save.png"
+    save.id = "black-save"
     const guardado = document.querySelector(".counter")
     const msg = document.createElement("p")
     msg.textContent = "Guardado en colección"
     msg.style.margin = "0"
     msg.style.textAlign = "right"
+    msg.style.color = "rgb(78, 167, 226)"
     guardado.insertAdjacentElement("beforebegin", msg)
-    setTimeout(e => {
-        msg; 2000
-    })
+    function disapear() {
+        msg.style.display = "none";
+    }
+    setTimeout(disapear, 1500)
 })
